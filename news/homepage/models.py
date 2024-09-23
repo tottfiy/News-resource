fb3c5ee91c0724b8aefdc2ffb214d638e6a3ef3e
tottfiy <ivanko.lisenko@gmail.com>
2024-09-24 02:25:07 +0300

    Created Databases for Authors and News, Created admin panel.

--- news/homepage/models.py
+++ news/homepage/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    name = models.CharField(max_length=150)
+    author = models.CharField(max_length=60)
+    date = models.DateTimeField(max_length=40)
+    text = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return (f'{self.name}, '
+                f'{self.author}, '
+                f'{self.date}, '
+                f'({self.text})')
+
+
+class Authors(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    short_text = models.TextField(max_length=300)
+
+    def __str__(self):
+        return (f'{self.name}, '
+                f'{self.surname}, '
+                f'{self.short_text}')
--- news/homepage/views.py
+++ news/homepage/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import News, Authors
 
 
 # Create your views here.
 def landing_handle(request):
     return render(request, 'homepage/landing.html')
+
+
+def get_news(request):
+    newslist = News.objects.all()
+    return render(request, 'homepage/landing.html', {'news': newslist})
+
+
+def get_authors(request):
+    authorslist = Authors.objects.all()
+    return render(request, 'homepage/landing.html', {'authors': authorslist})
